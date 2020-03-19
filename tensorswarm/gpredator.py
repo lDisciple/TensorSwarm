0d740cb8938ddcfc71e21f08db95b7939733fbce
ldisciple <jbotha.christ4life@gmail.com>
2020-03-19 22:05:58 +0200

    Added global predator

---
+++ tensorswarm/gpredator.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+from utils import *
+
+@tf.function
+def gpredator(f, bounds, particles=6, predators=1, dim=3,
+            iters=1000,
+            fear_factor=0.8, prey_vmax=0.2, fear_dist=0.1, pred_vmax=0.2,
+            w=0.9, c1=0.7, c2=0.7, c3=0.7):
+    # Init
+    x, v = init_particles(particles, dim, bounds)
+    pb = f(x)
+    pb_x = tf.identity(x)
+    gb = tf.constant(float('Inf'), dtype=tf.float16)
+    gb_x = tf.zeros([dim], dtype=tf.float16)
+
+    x_pred = init_predators(predators, dim, bounds)
+
+    # Constants
+    bound_dist = bounds[1] - bounds[0]
+    alpha = prey_vmax*bound_dist # Velocity at distance 0 for the predator
+    """
+    2/b is the distance where the prey starts running with a reasonable magnitude.
+    We scale it based on dimensions as more dimensions means bigger distances.
+    """
+    beta = 2/(tf.math.sqrt(tf.cast(dim, tf.float16))*fear_dist*bound_dist) 
+
+    # Main loop
+    i = tf.constant(0, tf.int64) 
+    while iters > i:
+        # Fitness
+        with tf.name_scope("fitness"):
+            fit = bounded_fitness(f, x, bounds)
+
+        # Update personal best
+        with tf.name_scope("personalbest"):
+            pb, pb_x = eval_personalbest(fit, x, pb, pb_x)
+
+        # Global best
+        with tf.name_scope("globalbest"):
+            gb_i = tf.argmin(pb)
+            gb = pb[gb_i]
+            gb_x = pb_x[gb_i]
+
+        # Movement
+        with tf.name_scope("movement"):
+            r1 = tf.random.uniform([particles, dim], dtype=tf.float16)
+            r2 = tf.random.uniform([particles, dim], dtype=tf.float16)
+
+            fear = fear_component(x, x_pred, alpha, beta, fear_factor)
+            cog  = r1*(pb_x-x)
+            soc  = r2*(gb_x-x)
+
+            v = w*v + c1*cog + c2*soc + fear
+
+            x += v
+            x_pred += predator_velocity(x_pred, gb, pred_vmax)
+        
+        # Summary
+        tf.summary.scalar("global_best", gb, step=i)
+        log_personalbest(pb, i)
+        log_diversity(x, i)
+
+        # Stopping condition
+        with tf.name_scope("stopping_cond"):
+            i += 1
+
+    return gb, gb_x, i
+if __name__ == '__main__':
+    from functions import *
+    import time
+
+    particles = 30
+    predators = 2
+    dim = 30
+    iters = 5000
+
+    writer = tf.summary.create_file_writer("../logs/gpredator_{}".format(time.strftime("%Y-%m-%d_%H:%M:%S")))
+    with writer.as_default():
+        val, x, i  = gpredator(f_abs, (-100,100),
+            particles=particles, dim=dim,
+            predators=predators,
+            iters=iters)
+        print(f"{val} @ {x} - step: {i}")
+    
--- tensorswarm/utils.py
+++ tensorswarm/utils.py
@@ -8,6 +8,10 @@ def init_particles(particles, dim, bounds):
     
     return x, v
 
+def init_predators(predators, dim, bounds):
+    return tf.random.uniform([predators, dim], dtype=tf.float16,
+                minval=bounds[0], maxval=bounds[1])
+
 @tf.function
 def bounded_fitness(f, x, bounds):
     fit = f(x)
@@ -47,3 +51,30 @@ def log_personalbest(pb, step):
         tf.summary.scalar("min", tf.math.reduce_min(pb), step=step)
         tf.summary.scalar("max", tf.math.reduce_max(pb), step=step)
         tf.summary.scalar("mean", tf.math.reduce_mean(pb), step=step)
+
+@tf.function
+def nearest_particle(x, swarm):
+    return swarm[tf.argmin(tf.norm(swarm-x, axis=1))]
+
+@tf.function
+def nearest_particles(x, swarm):
+    return tf.map_fn(lambda x: nearest_particle(x, swarm), x)
+
+@tf.function
+def fear_component(x, x_pred, alpha, beta, fear_factor):
+    r = tf.random.uniform(x.shape, dtype=tf.float16)
+    target = x - nearest_particles(x, x_pred) # Run from nearest predators
+    d = tf.norm(target, axis=1)
+    D = alpha*tf.exp(-beta*d)
+    comp = r*D*target/d
+    with tf.name_scope("bravery"):
+        mood = tf.random.uniform(x.shape[:1], dtype=tf.float16)
+        cond = tf.reshape(mood < fear_factor, (-1, 1)) # Runners = True
+        comp = tf.where(cond, x=comp, y=0)
+
+    return comp
+
+@tf.function
+def predator_velocity(x, best, vmax):
+    r = tf.random.uniform(x.shape, maxval=vmax, dtype=tf.float16)
+    return r*(best-x)
